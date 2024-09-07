a8cf8e1643546e31596aca3ae8de4350dc6a98dd
serdaraltin <seyupaltin@gmail.com>
2024-09-07 22:21:45 +0300

    new simple structure

---
+++ src/imu_interface/src/main.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  //setupt motion detection
+  mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
+  mpu.setMotionDetectionThreshold(1);
+  mpu.setMotionDetectionDuration(20);
+  mpu.setInterruptPinLatch(true);	// Keep it latched.  Will turn off when reinitialized.
+  mpu.setInterruptPinPolarity(true);
+  mpu.setMotionInterrupt(true);
+
+  Serial.println("");
+  delay(100);
+}
+
+void loop() {
+
+  //if(mpu.getMotionInterruptStatus()) {
+    /* Get new sensor events with the readings */
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    /* Print out the values */
+    Serial.print(">Accel-X:");
+    Serial.println(a.acceleration.x);
+    Serial.print(">Accel-Y:");
+    Serial.println(a.acceleration.y);
+    Serial.print(">Accel-Z:");
+    Serial.println(a.acceleration.z);
+    Serial.print(">Gyro-X:");
+    Serial.println(g.gyro.x);
+    Serial.print(">Gyro-Y:");
+    Serial.println(g.gyro.y);
+    Serial.print(">Gyro-Z:");
+    Serial.println(g.gyro.z);
+  //}
+
+  delay(10);
+}
